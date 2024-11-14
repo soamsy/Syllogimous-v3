3091e5bb41b4d4d3279162f9c112104fb1cf99e1
Soamsy <thsoams@gmail.com>
2024-11-14 16:45:46 -0700

    Space2D/3D: Adjust direction weights

--- js/generators/direction.js
+++ js/generators/direction.js
@@ -130,6 +130,13 @@ function pickWeightedRandomDirection(dirNames, dirCoords, baseWord, neighbors, w
             pool.push([name, dirCoord]);
             pool.push([name, dirCoord]);
             pool.push([name, dirCoord]);
+            pool.push([name, dirCoord]);
+            pool.push([name, dirCoord]);
+        } else if (distanceToClosest == 2) {
+            pool.push([name, dirCoord]);
+            pool.push([name, dirCoord]);
+            pool.push([name, dirCoord]);
+            pool.push([name, dirCoord]);
         } else {
             pool.push([name, dirCoord]);
             pool.push([name, dirCoord]);
