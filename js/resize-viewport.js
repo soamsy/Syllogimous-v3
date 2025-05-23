a0077b7d68103fec87e01333f608de36725060fd
Soamsy <thsoams@gmail.com>
2025-05-23 08:14:13 -0700

    Use svh for viewport instead of manually figuring it out

--- js/resize-viewport.js
+++
@@ -1,8 +0,0 @@
-
-const setViewportHeight = () => {
-  let px = window.innerHeight;
-  document.documentElement.style.setProperty('--viewport-height', `${px}px`);
-}
-
-window.addEventListener('resize', setViewportHeight);
-window.addEventListener('load', setViewportHeight);
